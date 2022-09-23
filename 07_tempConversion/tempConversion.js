f5a4e1945647e8d492b1d5eefe4835ef6052b4f2
Proja96 <108406302+Proja96@users.noreply.github.com>
2022-09-23 14:22:58 +0200

    uradjeni 5 i 6

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(numb) {
+  let cels =(5/9) * (numb - 32);
+  return Math.round(cels * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(numb) {
+  let faren = (numb * 9/5) + 32;
+  return Math.round(faren * 10) / 10;
 };
 
 // Do not edit below this line
